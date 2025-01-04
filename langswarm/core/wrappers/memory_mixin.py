7b32551f5a9e4ebeb97295ed4ea16d6612ac0a61
aekdahl <alexander.ekdahl@gmail.com>
2025-01-04 10:43:57 +0100

    Update memory_mixin.py

--- langswarm/core/wrappers/memory_mixin.py
+++ langswarm/core/wrappers/memory_mixin.py
@@ -13,6 +13,12 @@ class MemoryMixin:
     def _initialize_memory(self, agent: Any, memory: Optional[Any], in_memory: list) -> Optional[Any]:
         """
         Initialize or validate memory for the agent.
+
+        If the agent already have memory initialized, we used that.
+        If the memory is a LangChain memory instance, we use that.
+        If non of these are available we return None. No external memory in use.
+
+        :ToDo - Initialize LangChain memory (or other external memory) upon request.
         """
         if hasattr(agent, "memory") and agent.memory:
             return agent.memory
